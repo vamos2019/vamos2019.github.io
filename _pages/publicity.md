501d6f8d23f3306b299c8b6df2b5948464c1f968
Gilles Perrouin <gilles.perrouin@gmail.com>
2018-09-05 14:59:15 +0200

    Added Logo

---
+++ _pages/publicity.md
@@ -0,0 +1,11 @@
+---
+title: Publicity
+permalink:  /publicity/  
+
+   
+---
+
+#Vamos Logo 
+
+![here](https://vamos2019.github.io/img/vamos_logo.png)
+
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"The website is online ! ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/VAMOS-2019-website/",
+        "teaser":null},{
+        "title": "Call For Papers",
+        "excerpt":"The CFP is out and the PC list has been completed. ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Call-For-Papers/",
         "teaser":null}]
